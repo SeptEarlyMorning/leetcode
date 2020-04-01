39ccb1d3de49e24c5e0d687f7295a06f76bdedc2
SeptEarlyMorning <1790732099@qq.com>
2020-04-01 23:56:12 +0800

    冒泡及归并排序补充

--- 912. 排序数组/index.js
+++ 912. 排序数组/index.js
@@ -2,34 +2,60 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(arr) {
-  return quickSort(arr)
+var sortArray = function (arr) {
+  // bubbleSort(arr, 0, arr.length);
+  // mergeSort(arr, 0, arr.length);
+  selectSort(arr, 0, arr.length);
+  console.log(arr);
+
+  return arr;
 };
-//quicksort
-function quickSort(arr, left = 0, right = arr.length - 1) {
-if (left < right) {
-  let pivot = ~~((right + left)>>1)
-  let newPivot = partition(arr, pivot, left, right)
-  quickSort(arr, left, newPivot - 1)
-  quickSort(arr, newPivot + 1, right)
+
+// 交换数组中的两个数据
+const swap = (arr, a, b) => {
+  [arr[a], arr[b]] = [arr[b], arr[a]];
 }
-return arr
-}
-//partition
-function partition(arr, pivot, left, right) {
-let pivotValue = arr[pivot]
-let newPivot = left
-swap(arr, pivot, right)
-for (let i = left; i < right; i++) {
-  if (arr[i] < pivotValue) {
-    swap(arr, i, newPivot)
-    newPivot ++
+
+// 冒泡排序
+const bubbleSort = (arr, lo, hi) => {
+  while (lo < (hi = bubble(arr, lo, hi)));
+};
+
+const bubble = (arr, lo, hi) => {
+  let last = lo;
+  while (++lo < hi) {
+    if (arr[lo - 1] > arr[lo]) {
+      last = lo;
+      swap(arr, lo - 1, lo);
+    }
   }
-}
-swap(arr, right, newPivot)
-return newPivot
-}
-//swap
-function swap(arr, i, j) {
-[arr[i], arr[j]] = [arr[j], arr[i]]
-}
+  return last;
+};
+
+// 归并排序
+const mergeSort = (arr, lo, hi) => {
+  if (hi - lo < 2) {
+    return;
+  }
+  let mi = (lo + hi) >> 1;
+  mergeSort(arr, lo, mi);
+  mergeSort(arr, mi, hi);
+  merge(arr, lo, mi, hi);
+};
+
+const merge = (arr, lo, mi, hi) => {
+  let arr1 = [];
+  let loNum = lo, miNum = mi;
+  while (lo < miNum && mi < hi) {
+    arr[lo] < arr[mi] ? arr1.push(arr[lo++]) : arr1.push(arr[mi++]);
+  }
+  arr1 = arr1.concat(lo === miNum ? arr.slice(mi, hi) : arr.slice(lo, miNum));
+  arr.splice(loNum, hi - loNum, ...arr1);
+};
+
+// 选择排序
+const selectSort = (arr, lo, hi) => {
+
+};
+
+sortArray([3, 2, 1])
