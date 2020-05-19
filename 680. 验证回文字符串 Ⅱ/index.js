f4cb7ee44cb815eea6ff5898618e33a4798ce4f1
SeptEarlyMorning <1790732099@qq.com>
2020-05-20 01:29:55 +0800

    修改

--- 680. 验证回文字符串 Ⅱ/index.js
+++ 680. 验证回文字符串 Ⅱ/index.js
@@ -3,24 +3,26 @@
  * @return {boolean}
  */
 var validPalindrome = function (s) {
-  let i = 0;
-  let j = s.length - 1;
-  let key = 0;
-  while (i < j) {
-    if (s[i] !== s[j]) {
-      if (s[i + 1] !== s[j] && s[i] !== s[j - 1]) {
-        return false;
-      } else if (s[i] === s[j - 1] || s[i + 1] === s[j]) {
-        key++;
-      }
+  let l = 0, r = s.length - 1 // 创建双指针
+  while (l < r) {
+    if (s[l] !== s[r]) { // 转为判断删掉左右指针字符之一的字串，是否是回文串
+      return isPali(s, l + 1, r) || isPali(s, l, r - 1)
     }
-    if (key === 2) {
-      return false;
-    }
-    i++;
-    j--;
+    l++
+    r--
   }
-  return true;
+  return true
 };
 
-console.log(validPalindrome("abca"));
+function isPali(s, l, r) { // 辅助函数
+  while (l < r) { // 指针相遇 结束循环
+    if (s[l] !== s[r]) { // 一票否决
+      return false
+    }
+    l++ // 指针挪动，相互逼近
+    r--
+  }
+  return true // 没有遇到不同，返回true
+}
+
+console.log(validPalindrome("ebcbbececabbacecbbcbe"));
