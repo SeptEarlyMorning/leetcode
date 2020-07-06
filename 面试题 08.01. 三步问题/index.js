faa03b0a420a1addbf7275a6d58849c271382aa4
SeptEarlyMorning <1790732099@qq.com>
2020-07-06 17:13:04 +0800

    对于模的处理，修改

--- 面试题 08.01. 三步问题/index.js
+++ 面试题 08.01. 三步问题/index.js
@@ -9,11 +9,11 @@ var waysToStep = function (n) {
   }
   for (let i = 4; i <= n; i++) {
     /* 用 js 方法来写，会慢点但是一行搞定 */
-    // dp.push((dp.shift() + dp[0] + dp[1]) % 1000000007);
-    const temp = (dp[0] + dp[1] + dp[2]) % 1000000007;
+    // dp.push(((dp.shift() + dp[0]) % 1000000007 + dp[1]) % 1000000007);
+    const temp = (dp[0] + dp[1]) % 1000000007 + dp[2];
     dp[0] = dp[1];
     dp[1] = dp[2];
-    dp[2] = temp;
+    dp[2] = temp % 1000000007;
   }
   return dp[2];
 };
