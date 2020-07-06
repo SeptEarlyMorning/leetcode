d97d9cc9de6ca4d49dcea44352400016c855ed6b
SeptEarlyMorning <1790732099@qq.com>
2020-07-06 17:07:01 +0800

    LeetCode 面试题 08.01. 三步问题 2020/07/06

---
+++ 面试题 08.01. 三步问题/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var waysToStep = function (n) {
+  let dp = [1, 2, 4];
+  if (n < 4) {
+    return dp[n - 1];
+  }
+  for (let i = 4; i <= n; i++) {
+    /* 用 js 方法来写，会慢点但是一行搞定 */
+    // dp.push((dp.shift() + dp[0] + dp[1]) % 1000000007);
+    const temp = (dp[0] + dp[1] + dp[2]) % 1000000007;
+    dp[0] = dp[1];
+    dp[1] = dp[2];
+    dp[2] = temp;
+  }
+  return dp[2];
+};
