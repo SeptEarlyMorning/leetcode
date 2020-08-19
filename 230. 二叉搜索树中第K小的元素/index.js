2578d0baee45ec33aa5c4975185bc8dcb044414a
SeptEarlyMorning <1790732099@qq.com>
2020-08-19 21:13:32 +0800

    LeetCode 230. 二叉搜索树中第K小的元素 2020/08/19

---
+++ 230. 二叉搜索树中第K小的元素/index.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let num = -Infinity;
+
+  const dfs = (root) => {
+    if (!root || !k) return;
+    dfs(root.left);
+    --k;
+    if (!k) num = root.val;
+    dfs(root.right);
+  };
+  dfs(root);
+  return num;
+};
