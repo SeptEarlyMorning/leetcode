f521db5b2c600f804d160e95a3725c971313fd2d
SeptEarlyMorning <1790732099@qq.com>
2020-08-21 17:10:41 +0800

    LeetCode 111. 二叉树的最小深度 2020/08/21

---
+++ 111. 二叉树的最小深度/index.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+  if (!root) return 0;
+
+  const left = minDepth(root.left);
+  const right = minDepth(root.right);
+  if (left && right) return Math.min(left, right) + 1;
+  return Math.max(left, right) + 1;
+};
