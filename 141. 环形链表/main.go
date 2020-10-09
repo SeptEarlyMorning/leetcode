81539483c2ce47fb5daa033e9cc9160e13035b69
SeptEarlyMorning <1790732099@qq.com>
2020-10-09 14:33:47 +0800

    添加 go 语言解法

---
+++ 141. 环形链表/main.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	for slow, fast := head, head; fast != nil && fast.Next != nil; {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
