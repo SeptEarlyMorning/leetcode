d8bf15c41d37f5c228e47b88d704ee5e60ec4b19
SeptEarlyMorning <1790732099@qq.com>
2020-10-12 16:54:21 +0800

    LeetCode 783. 二叉搜索树节点最小距离 2020/10/12

---
+++ 783. 二叉搜索树节点最小距离/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "math"
+
+// TreeNode 二叉树
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+	min, pre := math.MaxInt64, -1
+
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil || min == 0 {
+			return
+		}
+		dfs(node.Left)
+		if pre != -1 {
+			min = getMinNum(node.Val-pre, min)
+		}
+		pre = node.Val
+		dfs(node.Right)
+	}
+
+	dfs(root)
+	return min
+}
+
+func getMinNum(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
