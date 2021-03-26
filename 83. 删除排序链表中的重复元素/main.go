850f0a7cbb1b77c2cb31573ded06a59ef6875120
SeptEarlyMorning <1790732099@qq.com>
2021-03-26 15:05:59 +0800

    LeetCode 83. 删除排序链表中的重复元素 2020/03/26

---
+++ 83. 删除排序链表中的重复元素/main.go
@@ -0,0 +1,27 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+  Val int
+  Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	cur := head
+
+	for cur != nil && cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return head
+}
